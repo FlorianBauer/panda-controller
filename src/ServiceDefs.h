13c0613431324138493435e34a2f9135cafbc42e
Florian Bauer <florian.bauer.dev@gmail.com>
2020-12-18 00:14:41 +0100

    Extended service definitions.

--- src/ServiceDefs.h
+++ src/ServiceDefs.h
@@ -1,11 +1,12 @@
 #ifndef SERVICEDEFS_H
 #define SERVICEDEFS_H
 
-constexpr char PANDA_ARM[] = "panda_arm";
-constexpr char PANDA_LINK0[] = "panda_link0";
-constexpr char PANDA_SERVICE_NAME[] = "panda_controller_service";
-constexpr char PANDA_CLIENT_NAME[] = "panda_controller_client";
-constexpr char SRV_MOVE_TO[] = "move_to";
-constexpr char SRV_SET_JOINTS[] = "set_joints";
+static constexpr char PANDA_ARM[] = "panda_arm";
+static constexpr char PANDA_LINK0[] = "panda_link0";
+static constexpr char PANDA_LINK_EEF[] = "panda_link8";
+static constexpr char PANDA_SERVICE_NAME[] = "panda_controller_service";
+static constexpr char PANDA_CLIENT_NAME[] = "panda_controller_client";
+static constexpr char SRV_MOVE_TO[] = "move_to";
+static constexpr char SRV_SET_JOINTS[] = "set_joints";
 
 #endif /* SERVICEDEFS_H */
